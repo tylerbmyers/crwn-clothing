457c073c4ebf7ffac47c879328e7cc8aea8cbad1
Tyler Myers <tyler.b.myers2@gmail.com>
2020-11-08 20:42:03 -0500

    removing redux-logger from production build

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -30,7 +30,9 @@ const Header = ({ currentUser, hidden }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/signin'></Link>
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
       )}
       <CartIcon />
     </div>
