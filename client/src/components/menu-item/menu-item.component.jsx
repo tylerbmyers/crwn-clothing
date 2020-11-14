250c51ba88237f79940bfc714189809fe4307fd9
Tyler Myers <tyler.b.myers2@gmail.com>
2020-11-14 18:11:12 -0500

    fix homepage routing

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -9,20 +9,25 @@ import {
   ContentSubtitle
 } from './menu-item.styles';
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <MenuItemContainer
-    size={size}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
-    <BackgroundImageContainer
-      className='background-image'
-      imageUrl={imageUrl}
-    />
-    <ContentContainer className='content'>
-      <ContentTitle>{title.toUpperCase()}</ContentTitle>
-      <ContentSubtitle>SHOP NOW</ContentSubtitle>
-    </ContentContainer>
-  </MenuItemContainer>
-);
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  console.log(linkUrl);
+  console.log(match);
+  console.log(history);
+  return (
+    <MenuItemContainer
+      size={size}
+      onClick={() => history.push(`/shop/${linkUrl}`)}
+    >
+      <BackgroundImageContainer
+        className='background-image'
+        imageUrl={imageUrl}
+      />
+      <ContentContainer className='content'>
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </MenuItemContainer>
+  );
+};
 
 export default withRouter(MenuItem);
